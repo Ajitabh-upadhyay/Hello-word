fd25c559e7675f2a271ad11cbd205643ccc6b345
Ajitabh-upadhyay <ajitabh@Ajitabhs-MacBook-Pro.local>
2017-03-05 17:52:32 +0530

    Initial Commit

---
+++ Hello GIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello GIT
+//
+//  Created by Ajitabh Upadhyay on 05/03/17.
+//  Copyright © 2017 Ajitabh Upadhyay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
